d8cadb341822027d5598345bd4853ef8ebf683eb
Shreya Desai <desai4@sheridancollege.ca>
2024-12-06 21:26:56 -0500

    added authentication repo, repo implementation, param models

---
+++ mobile-app/lib/features/analytics/data/repositories/auth_repo_impl.dart
@@ -0,0 +1,54 @@
+import 'package:dartz/dartz.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wqeqwewq/features/authentication/data/models/login_req_params.dart';
+
+import '../../../../resources/service_locator.dart';
+import '../../domain/repositories/auth_repo.dart';
+import '../data_sources/auth_api_service.dart';
+import '../models/signup_req_params.dart';
+
+class AuthRepoImpl extends AuthRepo {
+
+
+  @override
+  Future<Either> signup(SignupReqParams params) async {
+    var data = await sl<AuthService>().signup(params);
+    return data.fold(
+            (error) {
+          return Left(error);
+        },
+            (data) async {
+          final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+          sharedPreferences.setString('uid',data['uid']);
+          return Right(data);
+        }
+    );
+  }
+
+  @override
+  Future<Either> login(LoginReqParams params) async {
+    var data = await sl<AuthService>().login(params);
+    return data.fold(
+            (error) {
+          return Left(error);
+        },
+            (data) async {
+          final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+          sharedPreferences.setString('uid',data['uid']);
+          return Right(data);
+        }
+    );
+  }
+
+  @override
+  Future<bool> isAuthenticated() async {
+    final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    var token = sharedPreferences.getString('token');
+    if (token == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+}
