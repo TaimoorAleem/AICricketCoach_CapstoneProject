6a96587a0c9c564f3b774a94a8306d704ba4460e
Shreya Desai <desai4@sheridancollege.ca>
2025-04-05 19:21:14 -0400

    missed terraform commits

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -1,4 +1,4 @@
-resource "google_cloud_run_service" "container" {
+resource "google_cloud_run_service" "ApiContainer" {
   name     = "my-app-image"
   location = "us-central1"
 
@@ -6,12 +6,52 @@ resource "google_cloud_run_service" "container" {
     spec {
       containers {
         image = "gcr.io/aicc-proj-1/my-app-image:latest"
+        timeoutSeconds="500"
 
         env {
           name = "FIREBASE_CREDENTIALS"
           value_from {
             secret_key_ref {
               name = "firebase-creds"
+              key  = "1"
+            }
+          }
+        }
+      }
+    }
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/minScale" = "0"
+        "autoscaling.knative.dev/maxScale" = "5"
+      }
+    }
+  }
+}
+resource "google_cloud_run_service" "VpContainer" {
+  name     = "my-vp-app"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/aicc-proj-1/my-vp-app:v3"
+        timeoutSeconds="500"
+        resources {
+          limits {
+            cpu: "4000m"
+            memory: "2Gi"
+
+          }
+        }
+        startup_probe {
+          failure_threshold = "3"
+        }
+
+        env {
+          name = "ROBOFLOW_CREDENTIALS"
+          value_from {
+            secret_key_ref {
+              name = "roboflow-creds-creds"
               key  = "1"
             }
           }
