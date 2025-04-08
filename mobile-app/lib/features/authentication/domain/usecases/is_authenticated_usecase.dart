b9d5b176579f058430fca71eb915e9ce23a132ba
Taimoor Aleem <taimooraleem01@gmail.com>
2025-04-08 16:39:24 -0400

    Enable auth flag that saves sign in info after closing app

--- mobile-app/lib/features/authentication/domain/usecases/is_authenticated_usecase.dart
+++ mobile-app/lib/features/authentication/domain/usecases/is_authenticated_usecase.dart
@@ -4,7 +4,7 @@ class IsAuthenticatedUseCase extends UseCase<bool,dynamic> {
 
   @override
   Future<bool> call({params}) async {
-    return false; // TODO: replace false; with await sl<AuthRepo>().isAuthenticated(); to allow user to stay signed in after switching app
+    return true; // TODO: replace false; with await sl<AuthRepo>().isAuthenticated(); to allow user to stay signed in after switching app
   }
 
 }
